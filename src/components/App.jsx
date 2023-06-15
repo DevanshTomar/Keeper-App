0000747934a579caa17e617f9b7707e9b9745a17
DevanshTomar <tomardevansh83@gmail.com>
2023-06-15 15:21:05 -0700

    edit the Notes.jsx to take input as props and added the map function in App.jsx to crteate notes using the data in the notes.js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,20 @@ import React from 'react';
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
 
 
 function App(){
     return (
         <div>
             <Header />
-            <Note />
+            {notes.map(content =>
+                    (<Note 
+                        key = {content.id}
+                        title = {content.title}
+                        content = {content.content}
+                    />)
+            )}
             <Footer />
         </div>
     );
